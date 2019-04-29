38263eb344df46752aa529053b6ef2fb4518ba97
Ross Moody <fiftyfivehis@gmail.com>
2019-04-28 17:56:22 -0700

    rewrite in lots of ways

--- src/scripts/create-cards.js
+++ src/scripts/create-cards.js
@@ -1,44 +1,130 @@
-// import { createDownload } from './create-download'
 import { createButtons } from './create-buttons'
-import { closeIcon } from './icons'
+import { closeIcon, gobLogo } from './icons'
 
-function createElement(el, elClass) {
+export function noGobbles() {
+  const doc = document.querySelector('body')
+  const noGobbler = document.createElement('div')
+  noGobbler.classList.add('gob__noGobbler')
+  noGobbler.innerHTML = `😢 Drats, no SVGs to gobble `
+  doc.insertAdjacentElement('beforebegin', noGobbler)
+  setTimeout(() => {
+    noGobbler.remove()
+  }, 3000)
+}
+
+// Element creation helper
+function createElement(el, elClass, elPar = null, innH = null) {
   const i = document.createElement(el)
   i.className = elClass
-  return i
+  i.innerHTML = innH
+  return elPar ? elPar.appendChild(i) : i
+}
+
+// Tag helper
+function makeTag(string, tagClass, cont) {
+  const newTag = createElement('div', 'gob__tag')
+  newTag.innerHTML = string
+  newTag.classList.add(tagClass)
+  cont.appendChild(newTag)
+}
+
+// Scroll to top function
+function scrollToTop() {
+  const c = document.documentElement.scrollTop || document.body.scrollTop
+  if (c > 0) {
+    window.requestAnimationFrame(scrollToTop)
+    window.scrollTo(0, c - c / 8)
+  }
 }
 
 export function createCards(svgInfo) {
-  // Create container
-  const gobbler = createElement('div', 'gobbler')
+  scrollToTop()
+  const gobbler = createElement('div', 'gob')
   document.body.insertAdjacentElement('beforebegin', gobbler)
 
-  svgInfo.forEach(el => {
-    const gobblerCard = createElement('div', 'card')
-    const gobblerCardClone = createElement('div', 'card__clone')
-    const gobblerCardCloneWrapper = createElement('div', 'card__clone__wrapper')
-    const gobblerCardBtnContainer = createElement('div', 'card__btn-container')
+  // Create containers
+  const header = createElement('div', 'gob__header')
+  gobbler.appendChild(header)
+  const container = createElement('div', 'gob__container')
+  gobbler.appendChild(container)
+  const overlay = createElement('div', 'gob__overlay')
+  gobbler.appendChild(overlay)
+  const logoCont = createElement('div', 'gob__logoCont')
+  header.appendChild(logoCont)
+  const countCont = createElement('div', 'gob__countCont')
+  header.appendChild(countCont)
 
-    gobbler.appendChild(gobblerCard)
+  // Create header
+  createElement('div', 'gob__logo', logoCont, gobLogo)
+  createElement('h1', 'gob__title', logoCont, 'SVG Gobbler')
+  createElement('div', 'gob__count--svg', countCont, svgInfo.length)
+  createElement('h2', 'gob__count--title', countCont, 'SVGs to gobble')
+  createElement('div', 'gob__close', countCont, closeIcon)
+
+  // Close event listeners
+  document.querySelector('.gob__close').addEventListener('click', function() {
+    gobbler.remove()
+  })
+  document.querySelector('.gob__overlay').addEventListener('click', function() {
+    gobbler.remove()
+  })
+
+  // Smooth load header
+  setTimeout(() => {
+    header.classList.add('gob__header--show')
+  }, 50)
+
+  // Create cards
+  svgInfo.forEach((el, i) => {
+    // Create dom elements
+    const gobblerCard = createElement('div', 'gob__card')
+    container.appendChild(gobblerCard)
+    const gobblerCardClone = createElement('div', 'gob__card__svg')
+    const gobblerCardCloneWrapper = createElement(
+      'div',
+      'gob__card__svg__wrapper'
+    )
+    const gobblerCardFooter = createElement('div', 'gob__card__footer')
+    const gobblerCardBtns = createElement('div', 'gob__btns')
+
+    // Smooth card load
+    setTimeout(() => {
+      gobblerCard.classList.add('gob__card--show')
+    }, 60 * i)
     gobblerCard.appendChild(gobblerCardClone)
     gobblerCardClone.appendChild(gobblerCardCloneWrapper)
     gobblerCardCloneWrapper.appendChild(el.cleanXml)
-    gobblerCard.appendChild(gobblerCardBtnContainer)
+    gobblerCard.appendChild(gobblerCardFooter)
+    gobblerCardFooter.appendChild(gobblerCardBtns)
 
-    createButtons(gobblerCardBtnContainer, el)
-  })
+    // Make card tags
+    el.type ? makeTag(el.type, 'gob__tag--good', gobblerCardFooter) : null
+    el.type === 'sprite'
+      ? (function() {
+          const newTag = createElement('div', 'gob__tag--sprite')
+          gobblerCard.appendChild(newTag)
+        })()
+      : null
+    el.type === 'symbol'
+      ? (function() {
+          const newTag = createElement('div', 'gob__tag--symbol')
+          gobblerCard.appendChild(newTag)
+        })()
+      : null
+    el.inlineSize
+      ? makeTag('inline sizes', 'gob__tag--good', gobblerCardFooter)
+      : null
+    el.rects && el.rects !== 'hidden'
+      ? makeTag(el.rects, 'gob__tag--good', gobblerCardFooter)
+      : makeTag(el.rects, 'gob__tag--bad', gobblerCardFooter)
+    el.svgXml.hasAttribute('viewBox')
+      ? (function() {
+          const viewBox = el.svgXml.getAttribute('viewBox')
+          makeTag('viewBox', 'gob__tag--good', gobblerCardFooter)
+          makeTag(viewBox, 'gob__tag--good', gobblerCardFooter)
+        })()
+      : makeTag('no viewBox', 'gob__tag--bad', gobblerCardFooter)
 
-  // Add close button
-  const closeGobbler = createElement('div', 'gobbler__close')
-  closeGobbler.insertAdjacentHTML('beforeend', closeIcon)
-  closeGobbler.addEventListener('click', function() {
-    gobbler.remove()
+    createButtons(gobblerCardBtns, el)
   })
-  gobbler.insertAdjacentElement('beforeend', closeGobbler)
-  // Add overlay
-  const gobblerOverlay = createElement('div', 'gobbler__overlay')
-  gobblerOverlay.addEventListener('click', function() {
-    gobbler.remove()
-  })
-  gobbler.insertAdjacentElement('beforeend', gobblerOverlay)
 }
