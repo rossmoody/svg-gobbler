3e63e3ad64589992a1654daa3bcd4e5580009263
Ross Moody <fiftyfivehis@gmail.com>
2020-06-29 23:04:29 -0700

    eslint is working now

--- src/scripts/create-card.js
+++ src/scripts/create-card.js
@@ -1,76 +1,87 @@
-import download from './download-svgs'
+import download from './download-svgs';
 
 // Toggle success class
 function toggleSuccess(el, btnClass) {
-  el.classList.add('gob__btn--success')
-  el.classList.add(btnClass)
+  el.classList.add('gob__btn--success');
+  el.classList.add(btnClass);
   setTimeout(() => {
-    el.classList.remove('gob__btn--success')
-    el.classList.remove(btnClass)
-  }, 1500)
+    el.classList.remove('gob__btn--success');
+    el.classList.remove(btnClass);
+  }, 1500);
 }
 
 // Element creation helper
 function createElement(el, elClass, elPar = null, innH = null) {
-  const i = document.createElement(el)
-  i.className = elClass
-  i.innerHTML = innH
-  return elPar ? elPar.appendChild(i) : i
+  const i = document.createElement(el);
+  i.className = elClass;
+  i.innerHTML = innH;
+  return elPar ? elPar.appendChild(i) : i;
 }
 
 const hasAttr = (el) => {
-  let viewbox
-  let visible
-  el.svgXml.hasAttribute('viewBox')
-    ? (viewbox = 'gob__attrcont--viewbox')
-    : (viewbox = 'gob__display-none')
-  el.rects !== 'N/A'
-    ? (visible = 'gob__attrcont--hidden')
-    : (visible = 'gob__display-none')
-  return `<span class=${viewbox}></span><span class=${visible}></span>`
-}
+  let viewbox;
+  let visible;
+
+  if (el.svgXml.hasAttribute('viewBox')) {
+    viewbox = 'gob__attrcont--viewbox';
+  } else {
+    viewbox = 'gob__display-none';
+  }
+
+  if (el.rects !== 'N/A') {
+    visible = 'gob__attrcont--hidden';
+  } else {
+    visible = 'gob__display-none';
+  }
+
+  return `<span class=${viewbox}></span><span class=${visible}></span>`;
+};
 
 const createCards = (svgInfo, cont) => {
   // Create cards
   svgInfo.forEach((el, i) => {
     // Create dom elements
-    const gobblerCard = createElement('div', 'gob__card', cont)
-    const gobblerCardClone = createElement('div', 'gob__card__svg', gobblerCard)
+    const gobblerCard = createElement('div', 'gob__card', cont);
+    const gobblerCardClone = createElement(
+      'div',
+      'gob__card__svg',
+      gobblerCard
+    );
     const gobblerCardCloneWrapper = createElement(
       'div',
       'gob__card__svg__wrapper',
       gobblerCardClone
-    )
-    gobblerCardCloneWrapper.appendChild(el.cleanXml)
+    );
+    gobblerCardCloneWrapper.appendChild(el.cleanXml);
     const gobblerCardFooter = createElement(
       'div',
       'gob__card__footer',
       gobblerCard
-    )
-    const gobblerCardBtns = createElement('div', 'gob__btns', gobblerCardFooter)
+    );
+    const gobblerCardBtns = createElement(
+      'div',
+      'gob__btns',
+      gobblerCardFooter
+    );
 
     // Check if SVG has white attribute
-    el.hasWhite ? gobblerCardClone.classList.add('gob__card__svg--white') : null
+    if (el.hasWhite) {
+      gobblerCardClone.classList.add('gob__card__svg--white');
+    }
 
     // Smooth card load
     setTimeout(() => {
-      gobblerCard.classList.add('gob__card--show')
-    }, 60 * i)
+      gobblerCard.classList.add('gob__card--show');
+    }, 60 * i);
 
     // Create card warnings
-    el.type === 'sprite'
-      ? (() => {
-          const newTag = createElement('div', 'gob__tag--sprite')
-          gobblerCard.appendChild(newTag)
-        })()
-      : null
-
-    el.type === 'symbol'
-      ? (() => {
-          const newTag = createElement('div', 'gob__tag--symbol')
-          gobblerCard.appendChild(newTag)
-        })()
-      : null
+    if (el.type === 'sprite') {
+      const newTag = createElement('div', 'gob__tag--sprite');
+      gobblerCard.appendChild(newTag);
+    } else if (el.type === 'symbol') {
+      const newTag = createElement('div', 'gob__tag--symbol');
+      gobblerCard.appendChild(newTag);
+    }
 
     // Create card footer
     createElement(
@@ -78,52 +89,52 @@ const createCards = (svgInfo, cont) => {
       'gob__typecont',
       gobblerCardFooter,
       `<h4>Type</h4><h3>${el.type}</h3>`
-    )
+    );
 
     createElement(
       'div',
       'gob__sizecont',
       gobblerCardFooter,
       `<h4>Size</h4><h3>${el.rects}</h3>`
-    )
+    );
 
-    createElement('div', 'gob__attrcont', gobblerCardFooter, hasAttr(el))
+    createElement('div', 'gob__attrcont', gobblerCardFooter, hasAttr(el));
 
     if (el.svgString.length > 0) {
       // Download button
-      const downloadButton = createElement('button', 'gob__btn')
-      downloadButton.classList.add('gob__btn--download')
+      const downloadButton = createElement('button', 'gob__btn');
+      downloadButton.classList.add('gob__btn--download');
       downloadButton.addEventListener('click', () => {
-        toggleSuccess(downloadButton, 'gob__btn--success--download')
-        download.createRegDownload(el)
-      })
-      gobblerCardBtns.appendChild(downloadButton)
+        toggleSuccess(downloadButton, 'gob__btn--success--download');
+        download.createRegDownload(el);
+      });
+      gobblerCardBtns.appendChild(downloadButton);
 
       // Copy button
-      const copyButton = createElement('button', 'gob__btn')
-      copyButton.classList.add('gob__btn--copy')
+      const copyButton = createElement('button', 'gob__btn');
+      copyButton.classList.add('gob__btn--copy');
       copyButton.addEventListener('click', () => {
-        toggleSuccess(copyButton, 'gob__btn--success--copy')
-        download.copyRegClipboard(el)
-      })
-      gobblerCardBtns.appendChild(copyButton)
+        toggleSuccess(copyButton, 'gob__btn--success--copy');
+        download.copyRegClipboard(el);
+      });
+      gobblerCardBtns.appendChild(copyButton);
     } else {
       // adds alert to card
-      const newTag = createElement('div', 'gob__tag--cors')
-      gobblerCard.appendChild(newTag)
+      const newTag = createElement('div', 'gob__tag--cors');
+      gobblerCard.appendChild(newTag);
 
       // adds full width to button
-      gobblerCardBtns.classList.add('gob__btns--block')
+      gobblerCardBtns.classList.add('gob__btns--block');
 
       // Same-origin policies button. opens svg in new window
-      const corsBtn = createElement('a', 'gob__btn')
-      corsBtn.classList.add('gob__btn--cors-btn')
-      corsBtn.setAttribute('target', '_blank')
-      corsBtn.setAttribute('href', el.url)
+      const corsBtn = createElement('a', 'gob__btn');
+      corsBtn.classList.add('gob__btn--cors-btn');
+      corsBtn.setAttribute('target', '_blank');
+      corsBtn.setAttribute('href', el.url);
 
-      gobblerCardBtns.appendChild(corsBtn)
+      gobblerCardBtns.appendChild(corsBtn);
     }
-  })
-}
+  });
+};
 
-export default createCards
+export default createCards;
--- src/scripts/create-ui.js
+++ src/scripts/create-ui.js
@@ -1,14 +1,14 @@
-import JSZip from 'jszip'
-import FileSaver from 'file-saver'
-import { closeIcon, gobLogo } from './icons'
-import createCards from './create-card'
-import globalActions from './global'
+import JSZip from 'jszip';
+import FileSaver from 'file-saver';
+import { closeIcon, gobLogo } from './icons';
+import createCards from './create-card';
+import globalActions from './global';
 
 // Element creation helper
 function createElement(el, elClass) {
-  const i = document.createElement(el)
-  i.className = elClass
-  return i
+  const i = document.createElement(el);
+  i.className = elClass;
+  return i;
 }
 
 const struct = {
@@ -24,61 +24,61 @@ const struct = {
   count: 'gob__count--svg',
   downloadAll: 'gob__download',
   close: 'gob__close',
-}
+};
 
 const createUI = (svgInfo) => {
-  const gobbler = createElement('div', struct.globalContainer)
-  document.body.insertAdjacentElement('beforebegin', gobbler)
+  const gobbler = createElement('div', struct.globalContainer);
+  document.body.insertAdjacentElement('beforebegin', gobbler);
 
   // Create structure
-  const header = createElement('div', struct.header)
-  const container = createElement('div', struct.container)
-  const overlay = createElement('div', struct.overlay)
-  const logoCont = createElement('div', struct.logoContainer)
-  const countCont = createElement('div', struct.countContainer)
+  const header = createElement('div', struct.header);
+  const container = createElement('div', struct.container);
+  const overlay = createElement('div', struct.overlay);
+  const logoCont = createElement('div', struct.logoContainer);
+  const countCont = createElement('div', struct.countContainer);
 
   // Form structure
-  gobbler.appendChild(header)
-  gobbler.appendChild(container)
-  gobbler.appendChild(overlay)
-  header.appendChild(logoCont)
-  header.appendChild(countCont)
+  gobbler.appendChild(header);
+  gobbler.appendChild(container);
+  gobbler.appendChild(overlay);
+  header.appendChild(logoCont);
+  header.appendChild(countCont);
 
   // Create SVG Counter
   function isPlural() {
     return svgInfo.length === 1
       ? `Download ${svgInfo.length} SVG`
-      : `Download ${svgInfo.length} SVGs`
+      : `Download ${svgInfo.length} SVGs`;
   }
 
   // Create header
-  const gobLogoEl = createElement('div', struct.logo)
-  gobLogoEl.innerHTML = gobLogo
-  logoCont.appendChild(gobLogoEl)
-  const gobCount = createElement('button', struct.count)
-  gobCount.innerHTML = isPlural()
-  countCont.appendChild(gobCount)
+  const gobLogoEl = createElement('div', struct.logo);
+  gobLogoEl.innerHTML = gobLogo;
+  logoCont.appendChild(gobLogoEl);
+  const gobCount = createElement('button', struct.count);
+  gobCount.innerHTML = isPlural();
+  countCont.appendChild(gobCount);
   gobCount.addEventListener('click', () => {
-    const zip = new JSZip()
+    const zip = new JSZip();
     svgInfo.forEach((svg, index) => {
-      zip.file(`svg-${index}.svg`, svg.svgString)
-    })
+      zip.file(`svg-${index}.svg`, svg.svgString);
+    });
     zip.generateAsync({ type: 'blob' }).then(function (content) {
-      FileSaver.saveAs(content, 'gobbled_svgs.zip')
-    })
-  })
+      FileSaver.saveAs(content, 'gobbled_svgs.zip');
+    });
+  });
 
-  const gobClose = createElement('div', struct.close)
-  gobClose.innerHTML = closeIcon
-  countCont.appendChild(gobClose)
+  const gobClose = createElement('div', struct.close);
+  gobClose.innerHTML = closeIcon;
+  countCont.appendChild(gobClose);
 
   // Smooth load header
   setTimeout(() => {
-    header.classList.add('gob__header--show')
-  }, 80)
+    header.classList.add('gob__header--show');
+  }, 80);
 
-  createCards(svgInfo, container)
-  globalActions()
-}
+  createCards(svgInfo, container);
+  globalActions();
+};
 
-export default createUI
+export default createUI;
