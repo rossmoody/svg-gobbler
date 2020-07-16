91f07eafbffca675ee3287ad24eeaae27544be8c
Ross Moody <fiftyfivehis@gmail.com>
2020-07-16 16:02:56 -0700

    find base 64

--- src/scripts/find-svgs.js
+++ src/scripts/find-svgs.js
@@ -3,9 +3,7 @@ function findSVGs() {
   const objDatas = Array.from(
     window.document.querySelectorAll('object[data*=".svg"]')
   )
-  const imgSrcs = Array.from(
-    window.document.querySelectorAll('img[src*=".svg"]')
-  )
+  const imgSrcs = Array.from(window.document.querySelectorAll('img'))
   const pageDivs = Array.from(window.document.querySelectorAll('div'))
 
   const pageSVGs = [...svgTags, ...imgSrcs, ...objDatas, ...pageDivs]
