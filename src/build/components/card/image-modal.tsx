d71ed8ecc70155e12344722af097c8c5ad6459ae
rossmoody <fiftyfivehis@gmail.com>
2021-07-06 17:25:57 -0700

    add more values

--- src/build/components/card/image-modal.tsx
+++ src/build/components/card/image-modal.tsx
@@ -83,6 +83,9 @@ const ImageModal = ({
             <option value={1}>1x</option>
             <option value={2}>2x</option>
             <option value={3}>3x</option>
+            <option value={4}>4x</option>
+            <option value={5}>5x</option>
+            <option value={10}>10x</option>
           </Select>
           <Button
             colorScheme="red"
