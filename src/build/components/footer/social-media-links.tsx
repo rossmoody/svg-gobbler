27628384045f1e7c29f9f44961525960b58c0b68
rossmoody <fiftyfivehis@gmail.com>
2021-07-06 18:53:07 -0700

    Update social-media-links.tsx

--- src/build/components/footer/social-media-links.tsx
+++ src/build/components/footer/social-media-links.tsx
@@ -1,15 +1,9 @@
 import { ButtonGroup, ButtonGroupProps, IconButton } from '@chakra-ui/react'
 import * as React from 'react'
-import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa'
+import { FaGithub, FaTwitter } from 'react-icons/fa'
 
 export const SocialMediaLinks = (props: ButtonGroupProps) => (
   <ButtonGroup variant="ghost" color="gray.600" {...props}>
-    <IconButton
-      as="a"
-      href="https://www.linkedin.com/in/moodyrooster/"
-      aria-label="LinkedIn"
-      icon={<FaLinkedin fontSize="20px" />}
-    />
     <IconButton
       as="a"
       href="https://github.com/rossmoody/svg-gobbler"
