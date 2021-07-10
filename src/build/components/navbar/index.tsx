7ac6bc7d6b23ea8aa49651454b881b2a8ed05105
rossmoody <fiftyfivehis@gmail.com>
2021-07-10 11:18:24 -0700

    add feedback mechanism

--- src/build/components/navbar/index.tsx
+++ src/build/components/navbar/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Box, Flex, useColorModeValue, Divider } from '@chakra-ui/react'
+import { Box, Flex, useColorModeValue, Button } from '@chakra-ui/react'
+import { FeedbackFish } from '@feedback-fish/react'
 
 import Logo from './logo'
 import ThemeToggle from './theme-toggle'
@@ -9,7 +10,12 @@ const Navbar = () => {
     <Box p="8" bg={useColorModeValue('gray.800', 'gray.800')} as="nav">
       <Flex maxW="7xl" mx="auto" justifyContent="space-between">
         <Logo size={40} />
-        <ThemeToggle />
+        <Flex>
+          <FeedbackFish projectId="cc5d44e3563b88">
+            <Button marginRight={4}>Send feedback</Button>
+          </FeedbackFish>
+          <ThemeToggle />
+        </Flex>
       </Flex>
     </Box>
   )
