e9b43eede373ab6207c3dcdcaf5dd348a2aa5588
rossmoody <fiftyfivehis@gmail.com>
2021-07-15 16:18:44 -0700

    fix cycle dependency error

--- src/build/components/index.ts
+++ src/build/components/index.ts
@@ -1,5 +1,4 @@
-/* eslint-disable import/no-cycle */
 export { default as Toolbar } from './toolbar'
-export { default as Footer } from './footer'
 export { default as Gallery } from './gallery'
+export { default as Footer } from './footer'
 export { default as Navbar } from './navbar'
