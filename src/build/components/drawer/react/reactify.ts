e0b5b58ac6002f17479ca83c4c62fe01b63a0ca3
rossmoody <fiftyfivehis@gmail.com>
2021-12-02 20:54:01 -0800

    3.16

--- src/build/components/drawer/react/reactify.ts
+++ src/build/components/drawer/react/reactify.ts
@@ -1,11 +1,14 @@
 export async function reactify(svgString: string) {
-  const res = await fetch('https://stoisy.com', {
-    headers: {
-      'content-type': 'text/html',
-    },
-    method: 'post',
-    body: svgString,
-  })
+  const res = await fetch(
+    'https://us-central1-svg-gobbler.cloudfunctions.net/SVGR',
+    {
+      headers: {
+        'content-type': 'text/html',
+      },
+      method: 'post',
+      body: svgString,
+    }
+  )
 
   const json = await res.text()
   return json
