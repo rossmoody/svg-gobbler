01d42d1e1f246b6e7d196c28c562a26b860b0b9f
rossmoody <fiftyfivehis@gmail.com>
2022-05-10 22:05:59 -0700

    cleanup

--- src/build/components/index.ts
+++ src/build/components/index.ts
@@ -1,7 +1,6 @@
-export { default as DropZone } from './generic/drop-zone'
 export { default as Footer } from './footer'
+export { default as Gallery } from './gallery'
+export { default as DropZone } from './generic/drop-zone'
 export { default as Navbar } from './navbar'
 export { default as Toolbar } from './toolbar'
-export { default as Gallery } from './gallery'
-
 export { default as loc } from './utils/localization'
