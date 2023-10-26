2d48801c9d8ffdfa69ec22edd019ed449217616d
rossmoody <fiftyfivehis@gmail.com>
2023-10-25 21:45:14 -0700

    reset new build

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { crx } from '@crxjs/vite-plugin'
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+import manifest from './manifest.json'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), crx({ manifest})],
+})
