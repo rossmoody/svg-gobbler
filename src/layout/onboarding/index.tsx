d1c543474c6420c1c97b2eda93b586ec5d39c908
rossmoody <fiftyfivehis@gmail.com>
2023-11-27 22:31:22 -0800

    refactor

--- src/layout/onboarding/index.tsx
+++ src/layout/onboarding/index.tsx
@@ -3,7 +3,6 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline'
 import { gatherPageData } from 'scripts/gather-page-data'
 import { Logo } from 'src/components'
 import { links } from 'src/constants/links'
-import { GridPattern } from './grid-pattern'
 import graphic from './onboarding-graphic.png'
 
 export const OnboardingLayout = () => {
@@ -20,7 +19,29 @@ export const OnboardingLayout = () => {
 
   return (
     <div className="relative isolate flex min-h-screen items-center overflow-hidden bg-white">
-      <GridPattern />
+      <svg
+        className="absolute inset-0 -z-10 h-full w-full stroke-gray-200 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
+        aria-hidden="true"
+      >
+        <defs>
+          <pattern
+            id="0787a7c5-978c-4f66-83c7-11c213f99cb7"
+            width={200}
+            height={200}
+            x="50%"
+            y={-1}
+            patternUnits="userSpaceOnUse"
+          >
+            <path d="M.5 200V.5H200" fill="none" />
+          </pattern>
+        </defs>
+        <rect
+          width="100%"
+          height="100%"
+          strokeWidth={0}
+          fill="url(#0787a7c5-978c-4f66-83c7-11c213f99cb7)"
+        />
+      </svg>
       <div className="mx-auto max-w-7xl px-6 py-8 lg:flex lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl lg:flex-shrink-0 lg:pt-8">
           <Logo className="h-11" />
