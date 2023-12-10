ee0b2a77db43de56e08993383d6bf88d838b2d1e
rossmoody <fiftyfivehis@gmail.com>
2023-12-09 17:47:41 -0800

    run eslint

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,20 +6,22 @@ import radixPlugin from 'tailwindcss-radix'
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{ts,tsx}'],
-  plugins: [formsPlugin, headlessPlugin, radixPlugin, scrollbarPlugin],
   darkMode: 'class',
+  plugins: [formsPlugin, headlessPlugin, radixPlugin, scrollbarPlugin],
   theme: {
     extend: {
+      animation: {
+        'slide-down-fade': 'slide-down-fade 200ms ease-in-out',
+        'slide-left-fade': 'slide-left-fade 200ms ease-in-out',
+        'slide-right-fade': 'slide-right-fade 200ms ease-in-out',
+        'slide-up-fade': 'slide-up-fade 200ms ease-in-out',
+        'toast-hide': 'toast-hide 100ms ease-in forwards',
+        'toast-slide-in-bottom': 'toast-slide-in-bottom 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+        'toast-slide-in-right': 'toast-slide-in-right 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+        'toast-swipe-out-x': 'toast-swipe-out-x 100ms ease-out forwards',
+        'toast-swipe-out-y': 'toast-swipe-out-y 100ms ease-out forwards',
+      },
       keyframes: {
-        // Tooltip
-        'slide-up-fade': {
-          '0%': { opacity: 0, transform: 'translateY(2px)' },
-          '100%': { opacity: 1, transform: 'translateY(0)' },
-        },
-        'slide-right-fade': {
-          '0%': { opacity: 0, transform: 'translateX(-2px)' },
-          '100%': { opacity: 1, transform: 'translateX(0)' },
-        },
         'slide-down-fade': {
           '0%': { opacity: 0, transform: 'translateY(-2px)' },
           '100%': { opacity: 1, transform: 'translateY(0)' },
@@ -28,18 +30,25 @@ export default {
           '0%': { opacity: 0, transform: 'translateX(2px)' },
           '100%': { opacity: 1, transform: 'translateX(0)' },
         },
-        // Toast
+        'slide-right-fade': {
+          '0%': { opacity: 0, transform: 'translateX(-2px)' },
+          '100%': { opacity: 1, transform: 'translateX(0)' },
+        },
+        'slide-up-fade': {
+          '0%': { opacity: 0, transform: 'translateY(2px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
         'toast-hide': {
           '0%': { opacity: 1 },
           '100%': { opacity: 0 },
         },
-        'toast-slide-in-right': {
-          '0%': { transform: `translateX(calc(100% + 1rem))` },
-          '100%': { transform: 'translateX(0)' },
-        },
         'toast-slide-in-bottom': {
           '0%': { transform: `translateY(calc(100% + 1rem))` },
           '100%': { transform: 'translateY(0)' },
+        },
+        'toast-slide-in-right': {
+          '0%': { transform: `translateX(calc(100% + 1rem))` },
+          '100%': { transform: 'translateX(0)' },
         },
         'toast-swipe-out-x': {
           '0%': { transform: 'translateX(var(--radix-toast-swipe-end-x))' },
@@ -53,19 +62,6 @@ export default {
             transform: `translateY(calc(100% + 1rem))`,
           },
         },
-      },
-      animation: {
-        // Tooltip
-        'slide-up-fade': 'slide-up-fade 200ms ease-in-out',
-        'slide-right-fade': 'slide-right-fade 200ms ease-in-out',
-        'slide-down-fade': 'slide-down-fade 200ms ease-in-out',
-        'slide-left-fade': 'slide-left-fade 200ms ease-in-out',
-        // Toast
-        'toast-hide': 'toast-hide 100ms ease-in forwards',
-        'toast-slide-in-right': 'toast-slide-in-right 150ms cubic-bezier(0.16, 1, 0.3, 1)',
-        'toast-slide-in-bottom': 'toast-slide-in-bottom 150ms cubic-bezier(0.16, 1, 0.3, 1)',
-        'toast-swipe-out-x': 'toast-swipe-out-x 100ms ease-out forwards',
-        'toast-swipe-out-y': 'toast-swipe-out-y 100ms ease-out forwards',
       },
     },
   },
