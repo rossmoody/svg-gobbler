93d786ce358a30ed807a99d0d75a596a4aa0f928
rossmoody <fiftyfivehis@gmail.com>
2024-07-21 14:52:57 -0700

    fake navigating back

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,11 @@
-// Routes
 export * from './collection'
-export * from './dashboard'
-export * from './details'
-export * from './root'
-export * from './settings'
-
-// Loaders
 export * from './collection/loader'
+export * from './dashboard'
 export * from './dashboard/loader'
+export * from './details'
+
 export * from './details/loader'
+export * from './root'
 export * from './root/loader'
+export * from './settings'
 export * from './settings/loader'
