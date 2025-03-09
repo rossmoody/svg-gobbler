8cefa4d70530b0bff29731cfc8dd7c6d935e9553
rossmoody <fiftyfivehis@gmail.com>
2025-03-09 09:46:11 -0700

    fix rendering of card color

--- src/layout/top-bar/card-color-onboarding.tsx
+++ src/layout/top-bar/card-color-onboarding.tsx
@@ -1,10 +1,10 @@
 import * as Tooltip from '@radix-ui/react-tooltip'
 import clsx from 'clsx'
-import { PropsWithChildren, useMemo } from 'react'
+import { useMemo } from 'react'
 import { useCollection, useUser } from 'src/providers'
 import { loc } from 'src/utils/i18n'
 
-export const CardColorOnboarding = ({ children }: PropsWithChildren) => {
+export const CardColorOnboarding = () => {
   const { state: userState } = useUser()
   const { state: collectionState } = useCollection()
 
@@ -20,33 +20,31 @@ export const CardColorOnboarding = ({ children }: PropsWithChildren) => {
     return collectionHasSvgWithWhite && !userState.onboarding.viewedCardColor
   }, [collectionHasSvgWithWhite, userState.onboarding.viewedCardColor])
 
-  if (!shouldShowCardColorOnboarding) {
-    return children
+  if (shouldShowCardColorOnboarding) {
+    return (
+      <Tooltip.Root open>
+        <Tooltip.Trigger asChild>
+          <span className="absolute inset-x-0" />
+        </Tooltip.Trigger>
+        <Tooltip.Portal>
+          <Tooltip.Content
+            className={clsx(
+              'radix-side-top:animate-slide-down-fade',
+              'radix-side-right:animate-slide-left-fade',
+              'radix-side-bottom:animate-slide-up-fade',
+              'radix-side-left:animate-slide-right-fade',
+              'inline-flex items-center rounded-lg px-2 py-2',
+              'bg-gray-800 text-xs shadow-md dark:bg-white',
+              'max-w-[16rem] text-center text-white dark:text-gray-800',
+            )}
+            side="bottom"
+            sideOffset={20}
+          >
+            {loc('onboarding_card_color')}
+            <Tooltip.Arrow className="fill-current text-gray-800 dark:text-gray-200" />
+          </Tooltip.Content>
+        </Tooltip.Portal>
+      </Tooltip.Root>
+    )
   }
-
-  return (
-    <Tooltip.Root open>
-      <Tooltip.Trigger asChild>
-        <span className="absolute inset-x-0" />
-      </Tooltip.Trigger>
-      <Tooltip.Portal>
-        <Tooltip.Content
-          className={clsx(
-            'radix-side-top:animate-slide-down-fade',
-            'radix-side-right:animate-slide-left-fade',
-            'radix-side-bottom:animate-slide-up-fade',
-            'radix-side-left:animate-slide-right-fade',
-            'inline-flex items-center rounded-lg px-2 py-2',
-            'bg-gray-800 text-xs shadow-md dark:bg-white',
-            'max-w-[16rem] text-center text-white dark:text-gray-800',
-          )}
-          side="bottom"
-          sideOffset={20}
-        >
-          {loc('onboarding_card_color')}
-          <Tooltip.Arrow className="fill-current text-gray-800 dark:text-gray-200" />
-        </Tooltip.Content>
-      </Tooltip.Portal>
-    </Tooltip.Root>
-  )
 }
