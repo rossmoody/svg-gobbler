b057dfc651462dfa6422e5d7c5c4b30065a9070f
rossmoody <fiftyfivehis@gmail.com>
2025-04-03 13:34:59 -0700

    setup name change

--- src/layout/collection/card/svg-name.tsx
+++ src/layout/collection/card/svg-name.tsx
@@ -1,10 +1,25 @@
 import { useCollection } from 'src/providers'
 
 import { Transition } from '@headlessui/react'
+import { StorageUtils } from 'src/utils/storage-utils'
 import { CardData } from '.'
 
 export const SvgName = ({ data }: CardData) => {
-  const { state } = useCollection()
+  const { state, dispatch } = useCollection()
+
+  const handleChange = async (e: React.FormEvent<HTMLSpanElement>) => {
+    const name = (e.target as HTMLSpanElement).textContent
+      ?.replace(/[\r\n]+/g, ' ')
+      .replace(/\s+/g, ' ')
+      .trim()
+    if (!name || name === data.name) return
+
+    data.updateName(name)
+    let pageData = await StorageUtils.getPageData(state.collectionId)
+    pageData.data = pageData.data.map((svg) => (svg.id === data.id ? data : svg))
+    StorageUtils.setPageData(state.collectionId, pageData)
+    dispatch({ type: 'process-data' })
+  }
 
   return (
     <Transition
@@ -16,10 +31,11 @@ export const SvgName = ({ data }: CardData) => {
       leaveFrom="opacity-100 h-8"
       leaveTo="opacity-0 h-0"
       show={state.view.filters['show-name']}
-      className="h-8 w-full whitespace-nowrap px-2 pb-1 text-xs text-gray-500 dark:text-gray-400"
+      className="h-8 w-full whitespace-nowrap  px-2 pb-2 text-xs text-gray-500 dark:text-gray-400"
     >
       <span className="flex h-full w-full items-center justify-center">
         <span
+          onBlur={handleChange}
           suppressContentEditableWarning
           contentEditable="plaintext-only"
           className="max-w-full overflow-hidden rounded-sm p-1 hover:bg-gray-100 dark:hover:bg-gray-900"
