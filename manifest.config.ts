49d966ffb37317aab690206777677f5eec6eb33e
Ross Moody <fiftyfivehis@gmail.com>
2025-04-17 00:08:56 -0700

    update comments

--- manifest.config.ts
+++ manifest.config.ts
@@ -1,7 +1,6 @@
 import type { ManifestV3Export } from '@crxjs/vite-plugin'
 
 import packageJson from './package.json'
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import { serverEndpoint } from './src/constants/server-config'
 
